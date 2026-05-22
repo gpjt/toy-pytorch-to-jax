769e6c5c334e0ccc62e2301908b3880b75d7da2c
Giles Thomas <giles@giles.net>
2026-05-22 01:06:19 +0100

    Final JIT

---
+++ pure_jax_xor_final_jit.py
@@ -0,0 +1,97 @@
+import math
+import time
+
+import jax
+import jax.numpy as jnp
+
+jax.config.update("jax_platform_name", "cpu")
+
+
+data = [
+    ([0., 0.], [0]),
+    ([0., 1.], [1]),
+    ([1., 0.], [1]),
+    ([1., 1.], [0]),
+]
+
+
+def generate_layer_parameters(key, d_in, d_out):
+    weight_key, bias_key = jax.random.split(key)
+    root_k = math.sqrt(1. / d_in)
+    weights = (jax.random.uniform(weight_key, shape=(d_out, d_in)) * 2 * root_k) - root_k
+    biases = (jax.random.uniform(bias_key, shape=(d_out,)) * 2 * root_k) - root_k
+    return {
+        "weights": weights,
+        "biases": biases,
+    }
+
+
+@jax.jit
+def forward(layers, inputs):
+    x = inputs
+    for layer in layers:
+        x = jax.nn.sigmoid(
+            x @ layer["weights"].T + layer["biases"]
+        )
+    return x
+
+
+@jax.jit
+def step(layers, grads, learning_rate):
+    layers = jax.tree.map(
+        lambda p, g: p - g * learning_rate,
+        layers,
+        grads,
+    )
+    return layers
+
+
+@jax.jit
+def calculate_loss(layers, inputs, target):
+    result = forward(layers, inputs)
+    return ((result - target) ** 2).mean()
+
+
+@jax.jit
+def train_step(layers, inputs, targets, learning_rate):
+    loss, grads = jax.value_and_grad(calculate_loss)(layers, inputs, targets)
+    layers = step(layers, grads, learning_rate)
+    return layers, loss
+
+
+def main():
+    key = jax.random.key(42)
+
+    layer_1_key, layer_2_key = jax.random.split(key)
+    layers = [
+        generate_layer_parameters(layer_1_key, 2, 2),
+        generate_layer_parameters(layer_2_key, 2, 1),
+    ]
+
+    learning_rate = 0.1
+
+    start = time.time()
+    for epoch in range(10000):
+        losses = []
+
+        for x, y in data:
+            layers, loss = train_step(layers, jnp.array(x), jnp.array(y), learning_rate)
+            losses.append(loss.item())
+
+        if epoch % 1000 == 0:
+            avg_loss = sum(losses) / len(losses)
+            print(f"Loss at epoch {epoch}: {avg_loss:.6f}")
+
+    end = time.time()
+
+    print(f"Trained in {end - start:.3f}s")
+
+    print(f"Loss at end: {avg_loss:.6f}")
+
+    for x, y in data:
+        result = forward(layers, jnp.array(x))
+        print(f"{x=}: {result=}, {y=}")
+
+
+if __name__ == "__main__":
+    main()
